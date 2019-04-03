0c6b5eeb75cca5de43b96ca61eb969af251a79a7
Molnar Csilla <csilla83.molnar@gmail.com>
2019-04-03 13:18:49 +0300

    Update MainPersonGenerator.java

--- src/main/java/MainPersonGenerator.java
+++ src/main/java/MainPersonGenerator.java
@@ -1,9 +1,13 @@
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.logging.Level;
+
 public class MainPersonGenerator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainPersonGenerator.class);
+
 
     public static void main(String[] args) {
 
@@ -11,8 +15,12 @@ public class MainPersonGenerator {
                 new Person.Builder("FirstPerson", AgeGroup.CHILD).withHairColor(HairColor.BLACK)
                         .withEyeColor(EyeColor.BLUE).build();
 
-
-        LOGGER.info(child.getEyeColor().name());
+        if (child.getEyeColor().name().equals(EyeColor.BLUE)){
+            LOGGER.info("A blue eye child was created");
+        }
+        else{
+            LOGGER.info("A non blue eye child was created");
+        }
 
 
 
