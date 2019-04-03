cb492713c34508ec3adac3143d4254f8a80c07b2
Molnar Csilla <csilla83.molnar@gmail.com>
2019-04-03 11:44:33 +0300

    added a Manifest file
    added a Manifest file

--- src/main/java/MainPersonGenerator.java
+++ src/main/java/MainPersonGenerator.java
@@ -1,8 +1,8 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-public class Main {
+public class MainPersonGenerator {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainPersonGenerator.class);
 
     public static void main(String[] args) {
         Person child =
