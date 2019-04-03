19b7e94f62d51858584695f5db2ca2edfd8e21fd
Molnar Csilla <csilla83.molnar@gmail.com>
2019-04-03 13:31:05 +0300

    Update MainPersonGenerator.java

--- src/main/java/MainPersonGenerator.java
+++ src/main/java/MainPersonGenerator.java
@@ -14,12 +14,13 @@ public class MainPersonGenerator {
         Person child =
                 new Person.Builder("FirstPerson", AgeGroup.CHILD).withHairColor(HairColor.BLACK)
                         .withEyeColor(EyeColor.BLUE).build();
+        System.out.println("Let me see "+child.toString());
 
-        if (!child.getEyeColor().name().equals(EyeColor.BLUE.name())) {
+      /*  if (!child.getEyeColor().name().equals(EyeColor.BLUE.name())) {
             LOGGER.info("A non blue eye child was created");
         } else {
             LOGGER.info("A blue eye child was created");
-        }
+        }*/
 
 
     }
