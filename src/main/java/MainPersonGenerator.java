709e9642dbbf5dda8eb1a19e1d690c619ed72525
Molnar Csilla <csilla83.molnar@gmail.com>
2019-04-03 13:37:30 +0300

    Update MainPersonGenerator.java

--- src/main/java/MainPersonGenerator.java
+++ src/main/java/MainPersonGenerator.java
@@ -1,12 +1,9 @@
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.logging.Level;
 
 public class MainPersonGenerator {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainPersonGenerator.class);
+
 
 
     public static void main(String[] args) {
