54679057023de4bcddf0ec8b8f9027bcd76020b4
Molnar Csilla <csilla83.molnar@gmail.com>
2019-04-03 12:19:55 +0300

    Update MainPersonGenerator.java

--- src/main/java/MainPersonGenerator.java
+++ src/main/java/MainPersonGenerator.java
@@ -1,27 +1,19 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 public class MainPersonGenerator {
 
-   // private static final Logger LOGGER = LoggerFactory.getLogger(MainPersonGenerator.class);
+  
 
     public static void main(String[] args) {
-        System.out.println("Simple hello to try");
-    /*    Person child =
+
+        Person child =
                 new Person.Builder("FirstPerson", AgeGroup.CHILD).withHairColor(HairColor.BLACK)
                         .withEyeColor(EyeColor.BLUE).build();
-        LOGGER.info(child.toString());
 
 
-        Person youth =
-                new Person.Builder("SecondPerson", AgeGroup.YOUTH).withHairColor(HairColor.BLOND)
-                        .withGender(Gender.MALE).build();
-        LOGGER.info(youth.toString());
+        System.out.println("Simple hello to try"+child.getEyeColor());
 
-        Person adult =
-                new Person.Builder("ThirdPerson", AgeGroup.ADULT).withHairColor(HairColor.BROWN)
-                        .withDegreeLevel(DegreeLevel.MASTER).build();
-        LOGGER.info(adult.toString());
-*/
+
+
 
 
 
